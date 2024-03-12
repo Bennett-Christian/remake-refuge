9da60e4bc76a7b03292719197297157d40bcee2e
Bennett-Christian <156120476+Bennett-Christian@users.noreply.github.com>
2024-03-12 15:03:51 -0700

    update slider script
    revamp hero image slider

--- scripts.js
+++ scripts.js
@@ -1,8 +1,29 @@
 console.log("This is a test for my capstone project.");
 
-const slider = document.querySelector(".slider");
-const sliderBG = document.querySelector(".hero-slider-background");
+const slider = document.querySelector("input[type='range']");
 const sliderFG = document.querySelector(".hero-slider-foreground");
+const sliderBG = document.querySelector(".hero-slider-background");
+
+const sliderFGImageWidth = getComputedStyle(sliderFG).width.slice(
+    0,
+    getComputedStyle(sliderFG).width.indexOf("r")
+);
+
+slider.addEventListener("input", (e) => {
+    adjustImage(e.target);
+})
+
+function adjustImage(target) {
+    sliderFG.computedStyleMap.width = `${(target.value / 100) * sliderFGImageWidth}rem`
+}
+
+adjustImage(slider);
+
+
+
+
+
+
 
 // console.log(slider);
 
@@ -14,8 +35,8 @@ const sliderFG = document.querySelector(".hero-slider-foreground");
 //     ('.hero-slider-foreground').css('width', `${sliderPos}%`);
 // }
 
-document.getElementById("slider").addEventListener("change",  (e) => {
-    const sliderPos = e.target.value;
-    // Update the width of the foreground image
-    ('.hero-slider-foreground').css('width', `${sliderPos}%`)
-  })
+// document.getElementById("slider").addEventListener("change",  (e) => {
+//     const sliderPos = e.target.value;
+//     // Update the width of the foreground image
+//     ('.hero-slider-foreground').css('width', `${sliderPos}%`)
+//   })
