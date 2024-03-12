872bfdaac9bee59a49db7fb0bad514e214311c39
Bennett-Christian <156120476+Bennett-Christian@users.noreply.github.com>
2024-03-12 10:57:41 -0700

    update scripts.js
    add initial hero image slider functionality

--- scripts.js
+++ scripts.js
@@ -1,1 +1,21 @@
 console.log("This is a test for my capstone project.");
+
+const slider = document.querySelector(".slider");
+const sliderBG = document.querySelector(".hero-slider-background");
+const sliderFG = document.querySelector(".hero-slider-foreground");
+
+// console.log(slider);
+
+// slider.addEventListener("drag", sliderMove);
+
+// function sliderMove(e) {
+//     const sliderPos = e.target.value;
+//     console.log(e.target.value);
+//     ('.hero-slider-foreground').css('width', `${sliderPos}%`);
+// }
+
+document.getElementById("slider").addEventListener("change",  (e) => {
+    const sliderPos = e.target.value;
+    // Update the width of the foreground image
+    ('.hero-slider-foreground').css('width', `${sliderPos}%`)
+  })
