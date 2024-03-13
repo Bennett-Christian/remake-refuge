f5fff7efba386d0f79a5f552cab1a35522d00b8c
Bennett-Christian <156120476+Bennett-Christian@users.noreply.github.com>
2024-03-13 14:07:32 -0700

    populate additional pages
    Updated more.html, extra.html and credits.html with basic skeleton of navbar on an empty page. Removed pointless comments

--- scripts.js
+++ scripts.js
@@ -20,25 +20,3 @@ function adjustImage(target) {
 }
 
 adjustImage(slider);
-
-
-
-
-
-
-
-// console.log(slider);
-
-// slider.addEventListener("drag", sliderMove);
-
-// function sliderMove(e) {
-//     const sliderPos = e.target.value;
-//     console.log(e.target.value);
-//     ('.hero-slider-foreground').css('width', `${sliderPos}%`);
-// }
-
-// document.getElementById("slider").addEventListener("change",  (e) => {
-//     const sliderPos = e.target.value;
-//     // Update the width of the foreground image
-//     ('.hero-slider-foreground').css('width', `${sliderPos}%`)
-//   })
